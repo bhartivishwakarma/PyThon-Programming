5dc054066f969fa2cead54246ec418937d0b6875
bhartivishwakarma <bhartiv12301@gmail.com>
2026-02-02 12:28:30 +0530

    Add combined student generator with science and non-science student yield functionality

---
+++ generator/connecting.py
@@ -0,0 +1,17 @@
+def science_students(x):
+  for i in range(1,x+1):
+    yield i
+
+def non_science_students(x,y):
+  for i in range(x,y+1):
+    yield i
+  
+
+def combined_students():
+  yield from science_students(5)
+  yield from non_science_students(10,15)
+  yield from non_science_students(25,30)
+
+student_generator=combined_students()
+for student in student_generator:
+  print(student)
