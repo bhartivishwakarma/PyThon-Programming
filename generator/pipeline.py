e915f1dcfce4cfa7e83fca8dd96530284674f061
bhartivishwakarma <bhartiv12301@gmail.com>
2026-02-02 12:46:56 +0530

    Add course generator and student increment functionality

---
+++ generator/pipeline.py
@@ -0,0 +1,12 @@
+def course_generator():
+    yield ("Computer Science", 5)
+    
+    yield ("Art", 10)
+    yield ("Business", 15)
+def add_five_students(courses):
+  for course,num in courses:
+    yield (course,num+5)
+    
+increased_courses =add_five_students(course_generator())
+for course in increased_courses:
+  print(course)
